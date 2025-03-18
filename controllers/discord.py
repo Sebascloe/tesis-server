135884142b6deba7572f0911529f856e2fb5a543
Sebascloe <sebastian.romero.24.2004@gmail.com>
2025-03-18 16:32:41 -0500

    feat: upload archivos

--- controllers/discord.py
+++ controllers/discord.py
@@ -23,6 +23,7 @@ USERINFO_URL = "https://discord.com/api/users/@me"
 if not CLIENT_ID or not CLIENT_SECRET or not REDIRECT_URI:
     raise ValueError("Missing Discord OAuth credentials")
 
+
 @router.get("/auth/login1")
 def login():
     return {
@@ -31,8 +32,11 @@ def login():
         f"&response_type=code&scope=identify email"
     }
 
+
 @router.get("/auth/callback1", response_model=UserResponse)
-async def auth_callback(code: str, response: Response, session: Session = Depends(get_session)):
+async def auth_callback(
+    code: str, response: Response, session: Session = Depends(get_session)
+):
     async with httpx.AsyncClient() as client:
         token_data = {
             "client_id": CLIENT_ID,
@@ -40,12 +44,18 @@ async def auth_callback(code: str, response: Response, session: Session = Depend
             "grant_type": "authorization_code",
             "code": code,
             "redirect_uri": REDIRECT_URI,
-            "scope": "identify email"
+            "scope": "identify email",
         }
-        token_res = await client.post(TOKEN_URL, data=token_data, headers={"Content-Type": "application/x-www-form-urlencoded"})
+        token_res = await client.post(
+            TOKEN_URL,
+            data=token_data,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
 
         if token_res.status_code != 200:
-            raise HTTPException(status_code=400, detail="Failed to retrieve access token")
+            raise HTTPException(
+                status_code=400, detail="Failed to retrieve access token"
+            )
 
         token_json = token_res.json()
         access_token = token_json.get("access_token")
@@ -53,11 +63,15 @@ async def auth_callback(code: str, response: Response, session: Session = Depend
         if not access_token:
             raise HTTPException(status_code=400, detail="Invalid token response")
 
-        user_res = await client.get(USERINFO_URL, headers={"Authorization": f"Bearer {access_token}"})
+        user_res = await client.get(
+            USERINFO_URL, headers={"Authorization": f"Bearer {access_token}"}
+        )
         user_info = user_res.json()
 
     # Buscar usuario en la base de datos
-    existing_user = session.exec(select(Users).where(Users.email == user_info["email"])).first()
+    existing_user = session.exec(
+        select(Users).where(Users.email == user_info["email"])
+    ).first()
 
     if not existing_user:
         new_user = Users(
@@ -65,7 +79,7 @@ async def auth_callback(code: str, response: Response, session: Session = Depend
             email=user_info["email"],
             phone="",
             dni="",
-            distrito_id=None
+            distrito_id=None,
         )
         session.add(new_user)
         session.commit()
@@ -78,7 +92,7 @@ async def auth_callback(code: str, response: Response, session: Session = Depend
         value=access_token,
         httponly=True,
         secure=SECURE_COOKIE,
-        samesite="Lax"
+        samesite="Lax",
     )
 
     return existing_user
--- controllers/distrito.py
+++ controllers/distrito.py
@@ -1,31 +1,37 @@
-from fastapi import APIRouter , Depends
+from fastapi import APIRouter, Depends
 from schemas.distrito import DistritoCreate, DistrtoResponse
 from sqlmodel import Session
 from database import get_session
-from services.distrito import crete_distrito, get_all_distritos, get_one_distrito, update_one_distrito
+from services.distrito import (
+    crete_distrito,
+    get_all_distritos,
+    get_one_distrito,
+    update_one_distrito,
+)
 from typing import List
 
 router = APIRouter()
 
+
 @router.post("/distrito", response_model=DistrtoResponse)
-def crete_distrito_archivo_endpoint(distrito:DistritoCreate, session: Session = Depends(get_session)):
+def crete_distrito_archivo_endpoint(
+    distrito: DistritoCreate, session: Session = Depends(get_session)
+):
     return crete_distrito(session, distrito)
+
 
 @router.get("/distrito", response_model=List[DistrtoResponse])
 def get_distritos_endpoint(session: Session = Depends(get_session)):
     return get_all_distritos(session)
 
+
 @router.get("/distrito/{distrito_id}", response_model=DistrtoResponse)
 def get_distrito_endpoint(distrito_id: int, session: Session = Depends(get_session)):
     return get_one_distrito(session, distrito_id)
 
+
 @router.patch("/distrito/{distrito_id}", response_model=DistrtoResponse)
-def update_distrito_endpoint(distrito_id: int, distrito: DistritoCreate, session: Session = Depends(get_session)):
+def update_distrito_endpoint(
+    distrito_id: int, distrito: DistritoCreate, session: Session = Depends(get_session)
+):
     return update_one_distrito(session, distrito, distrito_id)
-
-
-
-
-
-
-
